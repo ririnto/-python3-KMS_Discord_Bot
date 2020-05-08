70a9c9a690cfb94448d892270d4aa2b282ef0027
RIRINTO <54238243+RIRINTO@users.noreply.github.com>
2020-05-08 21:21:51 +0900

    .

--- KMS_Bot.py
+++ KMS_Bot.py
@@ -6,6 +6,7 @@ from module.additional_options import *
 from module.defense_percentage_ignore import *
 from module.level import *
 from module.information1 import *
+from module.informaiton2 import *
 from module.hangang import *
 from module.logging import *
 from module.gambling import *
@@ -21,14 +22,16 @@ extension_list = ['module.help', 'module.simbol',
                   'module.linknunion',
                   'module.BodyAndMindTrainingCenter']
 
-todolist=['module.gambling','module.information','module.homepage']
+todolist = ['module.gambling', 'module.information', 'module.homepage']
 
 bot = commands.AutoShardedBot(command_prefix=PREFIX)
 bot.remove_command('help')
 
 for extension in extension_list:
     bot.load_extension(extension)
-#client = discord.Client()
+
+
+# client = discord.Client()
 
 
 @bot.event
@@ -49,16 +52,30 @@ async def on_message(message):
     if message.content.startswith("#"):
         logging_main(message)
 
-    if message.content.startswith("#정보") \
-            or message.content.startswith("#무릉") \
+    elif message.content.startswith("#정보"):
+        status = information_reader(message)
+        msg = message.content.split(" ")
+        if status is 1:
+            output = information_none()
+            await message.channel.send(embed=output)
+        elif status is 2:
+            path = information1(msg)
+            await message.channel.send(file=discord.File(path, filename=msg[1] + '.png'))
+            output = discord.Embed()
+            output.set_footer(text="https://maple.gg/u/%s" % msg[1])
+            await message.channel.send(embed=output)
+        else:
+            output = information_help()
+            await message.channel.send(embed=output)
+
+    elif message.content.startswith("#무릉") \
             or message.content.startswith("#시드") \
             or message.content.startswith("#더시드") \
             or message.content.startswith("#유니온") \
             or message.content.startswith("#업적"):
         msg = message.content.split(" ")
-        output = information_main(msg)
+        output = information2_main(msg)
         await message.channel.send(embed=output)
-
 
     if message.content.startswith("#골드") \
             or message.content.startswith("#애플") \
@@ -86,6 +103,7 @@ async def on_message(message):
 
 try:
     import keys
+
     key = keys.key
 except:
     key = ''
